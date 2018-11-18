ec5808bbf2ef44fbee57c2b72ea48c8326338bab
devrod <rodneycumming@gmail.com>
2018-11-18 13:18:53 +1300

    Updates

---
+++ precache-manifest.4d87fc928b2469506db10209712842d3.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = [
+  {
+    "revision": "58e707bef763fc89815a60a49c40c8eb",
+    "url": "/parallax-portfolio/static/media/resumeSix.58e707be.png"
+  },
+  {
+    "revision": "5e39a58fd9c75b8a324b",
+    "url": "/parallax-portfolio/static/css/main.8bb36a94.chunk.css"
+  },
+  {
+    "revision": "1ec91569a7ffd72b74c6",
+    "url": "/parallax-portfolio/static/js/1.1ec91569.chunk.js"
+  },
+  {
+    "revision": "f2d686806bacff8d4020",
+    "url": "/parallax-portfolio/static/js/runtime~main.f2d68680.js"
+  },
+  {
+    "revision": "ca22951358838567b6668b40b88fcdb0",
+    "url": "/parallax-portfolio/static/media/logo.ca229513.svg"
+  },
+  {
+    "revision": "9cd04089414e33881ad51eaaf10d2522",
+    "url": "/parallax-portfolio/static/media/cloud.9cd04089.svg"
+  },
+  {
+    "revision": "b1bde58cbf9eb12bb89eabba6f50cb2b",
+    "url": "/parallax-portfolio/static/media/resume.b1bde58c.png"
+  },
+  {
+    "revision": "c6dde47a2f87dc8aeeaf2185c3a675f8",
+    "url": "/parallax-portfolio/static/media/portfolio.c6dde47a.png"
+  },
+  {
+    "revision": "a686e1222fc1f5761c7bdc74cee2da28",
+    "url": "/parallax-portfolio/static/media/phone.a686e122.png"
+  },
+  {
+    "revision": "d53a70279a8018326ec3e65a12e24f3f",
+    "url": "/parallax-portfolio/static/media/email.d53a7027.png"
+  },
+  {
+    "revision": "6f0f54fb3b6daf2ca7f31fe5858971e4",
+    "url": "/parallax-portfolio/static/media/layer_1.6f0f54fb.svg"
+  },
+  {
+    "revision": "471bf2239fa46a60415eb89170d724a3",
+    "url": "/parallax-portfolio/static/media/layer_2.471bf223.svg"
+  },
+  {
+    "revision": "e0f44f2da02385136d645af555d1f6a1",
+    "url": "/parallax-portfolio/static/media/layer_3.e0f44f2d.svg"
+  },
+  {
+    "revision": "d76f88fac97a987fa73a1baba482d0ac",
+    "url": "/parallax-portfolio/static/media/layer_4.d76f88fa.svg"
+  },
+  {
+    "revision": "10fd1f92190fa43b091fd89bafc10498",
+    "url": "/parallax-portfolio/static/media/pique.10fd1f92.svg"
+  },
+  {
+    "revision": "53ead24efdd80af48877754697d4499d",
+    "url": "/parallax-portfolio/static/media/uproar.53ead24e.svg"
+  },
+  {
+    "revision": "77577941f9d32c2cf5c2acb6bd95aa9f",
+    "url": "/parallax-portfolio/static/media/spelling.77577941.svg"
+  },
+  {
+    "revision": "51b778663997bd247a70bef7bcb8b336",
+    "url": "/parallax-portfolio/static/media/snippets.51b77866.svg"
+  },
+  {
+    "revision": "f17d110d5c2701d47e927175493fec13",
+    "url": "/parallax-portfolio/static/media/todo.f17d110d.svg"
+  },
+  {
+    "revision": "8f178b628a65b655394de31291319410",
+    "url": "/parallax-portfolio/static/media/typing.8f178b62.svg"
+  },
+  {
+    "revision": "9dd36a2e3703f1152b48719a437c030e",
+    "url": "/parallax-portfolio/static/media/resume.9dd36a2e.pdf"
+  },
+  {
+    "revision": "5e39a58fd9c75b8a324b",
+    "url": "/parallax-portfolio/static/js/main.5e39a58f.chunk.js"
+  },
+  {
+    "revision": "b4f42ce76eecee4a8f33f4291f75fba7",
+    "url": "/parallax-portfolio/static/media/resumeSixteen.b4f42ce7.png"
+  },
+  {
+    "revision": "5a1a5947d5311e6f59ee978ba38179eb",
+    "url": "/parallax-portfolio/static/media/css3.5a1a5947.svg"
+  },
+  {
+    "revision": "23d81d6122e67c3d1eb787bd0ae2d178",
+    "url": "/parallax-portfolio/static/media/express.23d81d61.svg"
+  },
+  {
+    "revision": "0804e31ed83b8506db423f0841545090",
+    "url": "/parallax-portfolio/static/media/figma.0804e31e.svg"
+  },
+  {
+    "revision": "b1ae5bf022047a328ca18c4177bc1cc0",
+    "url": "/parallax-portfolio/static/media/git.b1ae5bf0.svg"
+  },
+  {
+    "revision": "6ee633d9b116a6ecf408fc4d4560e530",
+    "url": "/parallax-portfolio/static/media/html5.6ee633d9.svg"
+  },
+  {
+    "revision": "24edd28d7c7f62d4f8faa306d36f47ec",
+    "url": "/parallax-portfolio/static/media/illustrator.24edd28d.svg"
+  },
+  {
+    "revision": "42e31620aa32f13cf7849618fc4133b0",
+    "url": "/parallax-portfolio/static/media/indesign.42e31620.svg"
+  },
+  {
+    "revision": "bd0d10f2174693be3bff8e3c9cb2489a",
+    "url": "/parallax-portfolio/static/media/jquery.bd0d10f2.svg"
+  },
+  {
+    "revision": "959421b3b8247771c9d9d84c531433ba",
+    "url": "/parallax-portfolio/static/media/js.959421b3.svg"
+  },
+  {
+    "revision": "52783a2f8043e237279093badadc9cf1",
+    "url": "/parallax-portfolio/static/media/mongod.52783a2f.svg"
+  },
+  {
+    "revision": "9ff78edca80462209e0b23eecd03ea34",
+    "url": "/parallax-portfolio/static/media/mongodb.9ff78edc.svg"
+  },
+  {
+    "revision": "5f5cb2f38f36a69bc4eae9ccf68f743f",
+    "url": "/parallax-portfolio/static/media/nodejs.5f5cb2f3.svg"
+  },
+  {
+    "revision": "286a1b21265a5f4ef4d049487d0505b4",
+    "url": "/parallax-portfolio/static/media/photoshop.286a1b21.svg"
+  },
+  {
+    "revision": "f3a283055fbf08b8cef4ae30aa2b843b",
+    "url": "/parallax-portfolio/static/media/react.f3a28305.svg"
+  },
+  {
+    "revision": "90d7190f522c05aed09d2d0486ea51df",
+    "url": "/parallax-portfolio/static/media/redux.90d7190f.svg"
+  },
+  {
+    "revision": "0b6ac59fc7757c1cdc58840074e71ea5",
+    "url": "/parallax-portfolio/static/media/sass.0b6ac59f.svg"
+  },
+  {
+    "revision": "edaa26c0b60e81d790f04cfdb39c6d17",
+    "url": "/parallax-portfolio/static/media/vscode.edaa26c0.svg"
+  },
+  {
+    "revision": "982e0ecbbcfad621ec611a0a8acf0adb",
+    "url": "/parallax-portfolio/static/media/webpack.982e0ecb.svg"
+  },
+  {
+    "revision": "ac9c269a27f4fafc3f8bc57f6c10ba69",
+    "url": "/parallax-portfolio/static/media/bg.ac9c269a.svg"
+  },
+  {
+    "revision": "1ce69c9dd3a171c87382842792d2c82f",
+    "url": "/parallax-portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.549a453ac29eaa69bc6a87c3e87dcdb2.js"
+  "/parallax-portfolio/precache-manifest.4d87fc928b2469506db10209712842d3.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/parallax-portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
